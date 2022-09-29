4e8ab49e5243ba0484e0b0dd884559233e838d58
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-09-29 14:43:33 +0000

    added file

---
+++ circumference.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Lucas LeBlanc
+# Created on: Sep 2022
+# This program calculates the circumference of a circle
+#    with user input
+
+import constants
+
+
+def main():
+    # this function calculates circumference
+    # input
+    radius = int(input("Enter radius of the circle (mm): "))
+    # process
+    circumference = constants.TAU * radius
+    # output
+    print("Circumference is {} mm.".format(circumference))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Created by: Lucas LeBlanc
+# Created on: Sep 2022
+# This module contains constants
+
+TAU = 6.28
